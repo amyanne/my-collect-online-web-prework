549aecd7a4195ec9ee4ed6b49fe9359b8d34a433
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 02:58:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,26 +1,15 @@
 require 'pry'
 
 def my_collect(empty_array)
+  new_array = Array.new
 
 if block_given?
     i = 0 
    
   while i = empty_array.length 
-    yield empty_array[i]
+    yield new_array[i]
     i += 1
-    name = empty_array[i]
-    name = name.capitalize
-    name = name.split(" ").first 
-    name_array.push(name)
-    binding.pry
-    end
-  elsif empty_array.length > 0
-    while i = empty_array.length 
-    yield empty_array[i]
-    i += 1
-    name = empty_array[i]
-    name_array.push(name_array)
-  end
+
 end
-  name_array
+  new_array
 end
