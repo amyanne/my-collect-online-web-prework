228968ca6fa88409ea42ae29138ade7494e76821
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 03:01:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,16 +3,15 @@ require 'pry'
 def my_collect(empty_array)
   new_array = Array.new
 
-if block_given?
+  if block_given?
     i = 0 
-   
-  while i < empty_array.length 
-    yield empty_array[i]
-    new_array << empty_array[i]
-    i += 1
+    while i < empty_array.length 
+      # yield empty_array[i]
+      new_array << yield(empty_array[i])
+      i += 1
+    end
+  else
+    nil
   end
-else
-  nil
-end
   new_array
 end
