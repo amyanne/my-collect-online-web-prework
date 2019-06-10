70776c9b4ad13df1a17d47f88e899cff23da6283
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 03:01:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,7 +6,6 @@ def my_collect(empty_array)
   if block_given?
     i = 0 
     while i < empty_array.length 
-      # yield empty_array[i]
       new_array << yield(empty_array[i])
       i += 1
     end
