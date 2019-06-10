efede0ce689350a446e00d1f67c7467f2dbd3908
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 02:53:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,32 +1,27 @@
 require 'pry'
 
-def my_collect(array)
-  count = array.length
-  while count > 0
-    yield
-    count-=
+def my_collect(empty_array)
+
+  name_array = []
+  i = 0
+  if empty_array.include?(" ") && empty_array.length > 0
+  binding.pry
+  while i = empty_array.length 
+    yield empty_array[i]
+    i += 1
+    name = empty_array[i]
+    name = name.capitalize
+    name = name.split(" ").first 
+    name_array.push(name)
+    binding.pry
+    end
+  elsif empty_array.length > 0
+    while i = empty_array.length 
+    yield empty_array[i]
+    i += 1
+    name = empty_array[i]
+    name_array.push(name_array)
   end
 end
-
-#   name_array = []
-#   i = 0
-#   if empty_array.include?(" ") && empty_array.length > 0
-#   binding.pry
-#   while i = empty_array.length 
-#     yield empty_array[i]
-#     i += 1
-#     name = empty_array[i]
-#     name = name.capitalize
-#     name = name.split(" ").first 
-#     name_array.push(name)
-#     binding.pry
-#     end
-#   elsif empty_array.length > 0
-#     while i = empty_array.length 
-#     yield empty_array[i]
-#     i += 1
-#     name = empty_array[i]
-#     name_array.push(name_array)
-#   end
-# end
-#   name_array
+  name_array
+end
